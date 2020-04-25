d8b1972084b91a5ee69a790ef3c6a33abfbc499f
shinnosuke.yamaguchi <s1y1s0y1@yahoo.co.jp>
2020-04-25 03:19:31 +0000

    Procfile

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,6 +45,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'content' => 'required|max:191',
             'status' => 'required|max:10',
         ]);
 
@@ -96,6 +97,7 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'content' => 'required|max:191',
             'status' => 'required|max:10',
         ]);
         
