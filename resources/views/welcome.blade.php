e0e5d5718842235ede4ecf092b950dcaa9b5e40f
shinnosuke.yamaguchi <s1y1s0y1@yahoo.co.jp>
2020-05-02 07:17:36 +0000

    temporary

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,6 @@
 @section('content')
     @if (Auth::check())
         <div class="row">
-            <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
-                    </div>
-                </div>
-            </aside>
             <div class="col-sm-8">
                 @if (Auth::id() == $user->id)
                     {!! Form::open(['route' => 'tasks.store']) !!}
